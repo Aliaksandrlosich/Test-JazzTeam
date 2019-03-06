b81b31b7f93ad546213324054b233a612adc1c34
Sanyu <sa.los@mail.ru>
2019-03-07 00:15:17 +0200

    feat I take data from json

--- autorization/js/autorization.js
+++ autorization/js/autorization.js
@@ -1,9 +1,14 @@
 function validation() {
 	let login = document.getElementById("input_name").value;
-	let passw = document.getElementById("input_password").value;
-	console.log(login);
-	console.log(passw);
-	if(login == "Admin" && passw == "12345678")
+    let passw = document.getElementById("input_password").value;
+    let requestURL = "https://aliaksandrlosich.github.io/Test-JazzTeam/autorization/js/password.json";
+    let request = new XMLHttpRequest();
+    request.open("GET", requestURL);
+    request.responseType = "json";
+    request.send();
+	request.onload = function() {
+	let data = request.response;
+	if(login == data.login && passw == data.Password)
 	{
 		localStorage["KeyProfile"] = "Garlfield";
 		location="../profile/profile.html";
@@ -11,4 +16,5 @@ function validation() {
 	else {
 		alert("Wrong username or password");
 	}
+}
 }
