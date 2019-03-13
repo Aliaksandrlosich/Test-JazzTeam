726e428be78ac8c0d20d7f3969e33152e6ca0055
Sanyu <sa.los@mail.ru>
2019-03-13 20:48:48 +0200

    refactoring script

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 if (localStorage["KeyProfile"] == "Garlfield"){
 	 const linkProfile = `<li><a class="navigation_link" href="../profile/profile.html">Hi,Garlfield 
 	 <img class="avatar" 
-	 src="../info/img/photo.jpg" title="avatar" height="50px" width="80px"></a></li>`;
+	 src="../img/photo.jpg" title="avatar" height="50px" width="80px"></a></li>`;
 	 const node = document.getElementById("linkProfile");
 	 node.parentNode.removeChild(node);
 	 document.querySelector("#header").innerHTML += linkProfile;
