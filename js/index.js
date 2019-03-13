3d0027dd67c76bfc249a414db027a5095c0dfc6c
Sanyu <sa.los@mail.ru>
2019-03-14 01:54:15 +0200

    style:removing unnecessary block

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
-if (localStorage["KeyProfile"] == "Garlfield"){
-	 const linkProfile = `<li><a class="navigation_link" href="../profile/profile.html">Hi,Garlfield 
+if (localStorage["KeyProfile"] == "Garlfield"){//There is access => changes the authorization with the profile
+	const linkProfile = `<li><a class="navigation_link" href="../profile/profile.html">Hi,Garlfield 
 	 <img class="avatar" 
 	 src="../img/photo.jpg" title="avatar" height="50px" width="80px"></a></li>`;
 	 const node = document.getElementById("linkProfile");
