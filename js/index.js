34aac3eaa8f0e60d8eff810e1bc2b9b475843e6c
Sanyu <sa.los@mail.ru>
2019-03-15 11:43:16 +0200

    style: body moved to a separate doc

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ if (localStorage["KeyProfile"] == "Garlfield"){//There is access => changes the 
 	const linkProfile = `<li><a class="navigation_link" href="../profile/profile.html">Hi,Garlfield 
 	 <img class="avatar" 
 	 src="../img/photo.jpg" title="avatar" height="50px" width="80px"></a></li>`;
-	 const node = document.getElementById("linkProfile");
-	 node.parentNode.removeChild(node);
-	 document.querySelector("#header").innerHTML += linkProfile;
+	const node = document.getElementById("linkProfile");
+	node.parentNode.removeChild(node);
+	document.querySelector("#header").innerHTML += linkProfile;
 }
--- profile/js/exit.js
+++ profile/js/exit.js
@@ -1,4 +1,4 @@
-function exit() {
+function exit() { //exit profile, delete localStorage flag
 	delete localStorage["KeyProfile"]
 	document.location.replace("../autorization/autorization.html");
 }
